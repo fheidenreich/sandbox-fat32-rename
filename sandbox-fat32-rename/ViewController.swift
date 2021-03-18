4a8444268227ca4455b9d58e6e81de7c46c25acd
Florian Heidenreich <fheidenreich@gmail.com>
2021-03-18 10:59:35 +0100

    Initial Commit

---
+++ sandbox-fat32-rename/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sandbox-fat32-rename
+//
+//  Created by Florian on 18.03.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
